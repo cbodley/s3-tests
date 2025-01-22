7432a381cf4247a706eb7a047e2fbfb6ce50eebe
Casey Bodley <cbodley@redhat.com>
2025-01-22 11:16:13 -0500

    s3: mark sigv2 tests with fails_on_rgw
    Signed-off-by: Casey Bodley <cbodley@redhat.com>
    (cherry picked from commit e786aa4f4279e23cd510dd404321c575c64a3425)

--- s3tests_boto3/functional/test_s3.py
+++ s3tests_boto3/functional/test_s3.py
@@ -6597,12 +6597,14 @@ def test_cors_presigned_get_object_tenant():
         method='get_object',
     )
 
+@pytest.mark.fails_on_rgw
 def test_cors_presigned_get_object_v2():
     _test_cors_options_presigned_method(
         client=get_v2_client(),
         method='get_object',
     )
 
+@pytest.mark.fails_on_rgw
 def test_cors_presigned_get_object_tenant_v2():
     _test_cors_options_presigned_method(
         client=get_v2_tenant_client(),
@@ -6622,12 +6624,14 @@ def test_cors_presigned_put_object_with_acl():
         cannedACL='private',
     )
 
+@pytest.mark.fails_on_rgw
 def test_cors_presigned_put_object_v2():
     _test_cors_options_presigned_method(
         client=get_v2_client(),
         method='put_object',
     )
 
+@pytest.mark.fails_on_rgw
 def test_cors_presigned_put_object_tenant_v2():
     _test_cors_options_presigned_method(
         client=get_v2_tenant_client(),
