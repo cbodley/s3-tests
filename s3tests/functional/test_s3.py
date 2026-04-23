b0c3705f84acb9b58634cc7206d88cb12992811d
Casey Bodley <cbodley@redhat.com>
2026-04-23 12:39:56 -0400

    s3: add test_head_object_404_with_policy_prefix()
    Fixes: https://tracker.ceph.com/issues/74398
    
    Signed-off-by: Casey Bodley <cbodley@redhat.com>
    (cherry picked from commit ee1ca235d94f9ed272878943ae6991ddaf60763b)
    
    Conflicts:
    	s3tests/functional/test_s3.py missing lots of tests

--- s3tests/functional/test_s3.py
+++ s3tests/functional/test_s3.py
@@ -18841,3 +18841,31 @@ def test_delete_objects_version_if_match_size():
 
     response = client.delete_objects(Bucket=bucket, Delete={'Objects': [{'Key': key, 'VersionId': version, 'Size': badsize}]})
     assert 200 == response['ResponseMetadata']['HTTPStatusCode']
+
+def test_head_object_404_with_policy_prefix():
+    client = get_client()
+    bucket = get_new_bucket(client)
+
+    policy = json.dumps({
+        "Version": "2012-10-17",
+        "Statement": [{
+            "Effect": "Allow",
+            "Principal": {"AWS": "*"},
+            "Action": "s3:ListBucket",
+            "Resource": f"arn:aws:s3:::{bucket}",
+            "Condition": {
+                "StringLike": {
+                    "s3:prefix": "public/*"
+                }
+            }
+        }]
+    })
+    client.put_bucket_policy(Bucket=bucket, Policy=policy)
+
+    alt_client = get_alt_client()
+    # expect 404 NoSuchKey for names that match the s3:prefix
+    e = assert_raises(ClientError, alt_client.head_object, Bucket=bucket, Key='public/object')
+    assert 404 == _get_status(e.response)
+    # expect 403 Forbidden for names that don't match
+    e = assert_raises(ClientError, alt_client.head_object, Bucket=bucket, Key='private/object')
+    assert 403 == _get_status(e.response)
